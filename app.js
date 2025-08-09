f066f837a7a6906db9b0468d72a8fbfca6b15e61
andreatrasvina <alucerotrasvina@gmail.com>
2025-08-08 23:43:31 -0700

    introduccion mastering gsap animation

---
+++ app.js
@@ -0,0 +1,55 @@
+// gsap.to('.box', {
+//     x: 300,
+//     y: 100,
+//     duration: 2,
+//     scale: 2,
+//     width: 200,
+//     rotate: 180,
+//     // delay: 2,
+//     backgroundColor: 'purple',
+
+// })
+
+// gsap.from('.box', {
+//     x: 300,
+//     y: 300,
+//     rotate: 500,
+//     duration: 2,
+//     backgroundColor: 'white',
+//     repeat: 2,
+//     yoyo: true, 
+// })
+
+gsap.to('.box', {
+    x: 500,
+    duration: 2, 
+    backgroundColor: 'pink',
+    stagger: 1,
+})
+
+// gsap.to('.box1', {
+//     x: 500,
+//     duration: 2, 
+//     backgroundColor: 'pink',
+// })
+
+// gsap.to('.box2', {
+//     x: 500,
+//     duration: 2, 
+//     backgroundColor: 'pink',
+//     delay: 2
+// })
+
+// gsap.to('.box3', {
+//     x: 500,
+//     duration: 2, 
+//     backgroundColor: 'pink',
+//     delay: 4
+// })
+
+// gsap.to('.box4', {
+//     x: 500,
+//     duration: 2, 
+//     backgroundColor: 'pink',
+//     delay: 6
+// })
